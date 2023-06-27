586260f481ee9ffd05e90d574372ab0880392c47
jinach0i <ginach0irk@gmail.com>
2023-06-27 19:48:57 +0900

    feat:Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return(
+        <header id="header">
+            <h1><a href="#"><img src="" alt="" /></a></h1>
+            <nav id="nav">
+                asd
+            </nav>
+        </header>
+    )
+};
